b7bf308d99580d41c056922a84916ca0ec0fca8f
권준희 <june@dhcp-8435.meeting.ietf.org>
2022-11-06 18:21:09 +0900

    skip connection

--- build/lib/pygcn/models.py
+++ build/lib/pygcn/models.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from layers import GraphConvolution
 
-
+# gcn layer varies from 2 to 7 for HW3-1
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
@@ -21,13 +21,13 @@ class GCN(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = F.relu(self.gc2(x, adj))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu(self.gc3(x, adj))
+        x = F.relu(self.gc3(x, adj))+x
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu(self.gc4(x, adj))
+        x = F.relu(self.gc4(x, adj))+x
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu(self.gc5(x, adj))
+        x = F.relu(self.gc5(x, adj))+x
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu(self.gc6(x, adj))
+        x = F.relu(self.gc6(x, adj))+x
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc7(x, adj)
         return F.log_softmax(x, dim=1)
