3243a5f5be4492e0cf8795f2f1b67727a9aeebc0
권준희 <june@gwonjunhuis-MacBook-Air.local>
2022-11-13 15:43:25 +0900

    done

---
+++ pygcn/models_case4.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from layers import GraphConvolution
+
+# gcn layer varies from 2 to 7 for HW3-1
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nhid)
+        self.gc3 = GraphConvolution(nhid, nhid)
+        self.gc4 = GraphConvolution(nhid, nhid)
+        self.gc5 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc2(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc3(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc4(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc5(x, adj)
+        return F.log_softmax(x, dim=1)
---
+++ pygcn/models_case6_skip2.py
@@ -0,0 +1,33 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from layers import GraphConvolution
+
+# gcn layer varies from 2 to 7 for HW3-1
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nhid)
+        self.gc3 = GraphConvolution(nhid, nhid)
+        self.gc4 = GraphConvolution(nhid, nhid)
+        self.gc5 = GraphConvolution(nhid, nhid)
+        self.gc6 = GraphConvolution(nhid, nhid)
+        self.gc7 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        x = F.relu(self.gc1(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc2(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc3(x, adj))+x
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc4(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc5(x, adj))+x
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.relu(self.gc6(x, adj))
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gc7(x, adj)
+        return F.log_softmax(x, dim=1)
